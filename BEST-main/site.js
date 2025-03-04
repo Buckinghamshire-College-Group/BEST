0fd73023d0916020ec877449192ce8dfc046d0ce
ellie 3 <elliewalker896@gmail.com>
2025-03-04 09:59:54 +0000

    Update site.js

--- BEST-main/site.js
+++ BEST-main/site.js
@@ -13,3 +13,36 @@ window.onclick = function(e) {
     }
   }
 }
+
+// Function to toggle the dropdown menu in the navbar
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Shows Support for choices
+function showSupport() {
+  const employmentStatus = document.getElementById('employmentStatus').value;
+  const workStatus = document.getElementById('workStatus').value;
+  const barriers = document.getElementById('barriers').value;
+  let resultText = "";
+
+  // Unemployed users
+  if (employmentStatus === 'unemployed') {
+    if (workStatus === 'notWorkedInNineMonths' && barriers === 'disability') {
+      resultText = "We recommend the following support options for you: Routes to Work, Skills Bootcamps, Supported Internships.";
+    } else {
+      resultText = "We recommend the following support options for you: Routes to Work, Skills Bootcamps.";
+    }
+  }
+
+  // Empolyed users
+  if (employmentStatus === 'employed') {
+    if (workStatus === 'worked' && barriers === 'noBarriers') {
+      resultText = "We recommend the following support options for you: Buckinghamshire Council jobs site, Skills Bootcamps.";
+    }
+  }
+
+  // Display the result
+  document.getElementById('supportResult').innerHTML = resultText;
+}
+
