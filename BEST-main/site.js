12b4af3cc10cbc840bfe9874ed9a73be1d34e096
Vlad <vladprisacaru820@gmail.com>
2025-10-15 23:46:02 +0100

    clean up
    1. placed the header into a separate html file and used as a reusable element for scalability and redundancy and avoid copy pasting
    2. moved all of the java script scripts into an external file to improve scalability and redundancy and avoid copy pasting
    3. removed duplicated file to clean up the folder and avoid un-needed clutter
    4. minor text changes and button "Get Support" style

--- BEST-main/site.js
+++ BEST-main/site.js
@@ -19,25 +19,72 @@ window.onclick = function(event) {
   }
 }
 
+function openLoginModal() {
+    document.getElementById("loginModal").style.display = "block";
+  }
+
+
+  function closeLoginModal() {
+    document.getElementById("loginModal").style.display = "none";
+  }
+
+  
+  function showSignup() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("signupForm").style.display = "block";
+  }
+
+  
+  function showLogin() {
+    document.getElementById("signupForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "block";
+  }
+
+
+  window.onclick = function(event) {
+    var modal = document.getElementById("loginModal");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
 
 function showSupport() {
-  const employmentStatus = document.getElementById('employmentStatus').value;
-  const workStatus = document.getElementById('workStatus').value;
-  const barriers = document.getElementById('barriers').value;
-  let resultText = "";
+      const employmentStatus = document.getElementById("employmentStatus").value;
+      const workStatus = document.getElementById("workStatus").value;
+      const barriers = document.getElementById("barriers").value;
 
-  if (employmentStatus === 'unemployed') {
-    if (workStatus === 'notWorkedInNineMonths' && barriers === 'disability') {
-      resultText = "We recommend the following support options for you: Routes to Work, Skills Bootcamps, Supported Internships.";
-    } else {
-      resultText = "We recommend the following support options for you: Routes to Work, Skills Bootcamps.";
+      let supportPage = "";
+
+      //Barriers
+      switch (barriers) {
+        case "drugs":
+        case "alcohol":
+          supportPage = "./drugs.html";
+          break;
+        case "mentalHealth":
+        case "disability":
+          supportPage = "./MentalSupport.html";
+          break;
+      }
+
+      if (!supportPage) {
+        if (employmentStatus === "unemployed") {
+          supportPage = "./WelfareSupport.html";
+        } else if (employmentStatus === "employed") {
+          if (["lastMonth", "last3Months", "last9Months"].includes(workStatus)) {
+            supportPage = "./UpskillingSupport.html";
+          } else if (workStatus === "firedQuit") {
+            supportPage = "./EmploymentSupport.html";
+          } else {
+            supportPage = "./BusinessSupport.html";
+          }
+        }
+      }
+
+      if (supportPage) {
+        window.location.href = supportPage;
+      } else {
+        alert("Please select all required fields to get support.");
+      }
     }
-  }
-
-  if (employmentStatus === 'employed') {
-    if (workStatus === 'worked' && barriers === 'noBarriers') {
-      resultText = "We recommend the following support options for you: Buckinghamshire Council jobs site, Skills Bootcamps.";
-    }
-  }
-
- 
